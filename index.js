c7583f243db5a4e80114a1db79a695a5fb72ea9d
vmklv <vsmarkelov@gmail.com>
2023-12-02 15:51:12 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const path = require('path')
+const exphbs = require('express-handlebars')
+
+const app = express()
+
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('veiws', 'views')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Главная страница',
+    isHome: true
+  })
+})
+
+app.get('/courses', (req, res) => {
+  res.render('courses', {
+    title: 'Курсы',
+    isCourses: true
+  })
+})
+
+app.get('/add', (req, res) => {
+  res.render('add', {
+    title: 'Добавить курс',
+    isAdd: true
+  })
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log('Server is running on port ${PORT}')
+})
